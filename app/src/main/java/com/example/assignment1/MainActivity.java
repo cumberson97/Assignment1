7aed9b65701a3695c07035dd4a9b21005afed031
shane12345601 <shane12345601@gmail.com>
2021-10-14 19:28:30 -0400

    Added Cart Page Floating Cart Button

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -2,20 +2,29 @@ package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView cakePic;
 
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+    }
+
+    public void goToCart(View view){
+        intent = new Intent(MainActivity.this, ShoppingCart.class);
+        startActivity(intent);
     }
 
     public void switchPic(View view) {
---
+++ app/src/main/java/com/example/assignment1/ShoppingCart.java
@@ -0,0 +1,24 @@
+package com.example.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ShoppingCart extends AppCompatActivity {
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+
+    public void goToShop(View view){
+        intent = new Intent(ShoppingCart.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
