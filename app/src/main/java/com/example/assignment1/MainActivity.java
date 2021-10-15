8c976b8317a42363a34fe3efa8f675151ee9c046
shane12345601 <shane12345601@gmail.com>
2021-10-14 20:19:02 -0400

    Added Cart counter Added switch case for each button

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView cakePic;
@@ -22,9 +24,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.home_page);
     }
 
-    public void goToCart(View view){
+    public void goToCart(View view) {
         intent = new Intent(MainActivity.this, ShoppingCart.class);
         startActivity(intent);
+    }
+
+    public void addToCart(View view) {
+        int btn = view.getId();
+
+        /*Toast toast = Toast.makeText(this, Integer.toString(btn), Toast.LENGTH_SHORT);
+        toast.setMargin(50,50);
+        toast.show();*/
+
+        TextView counter = findViewById(R.id.itemCounter);
+        String text = counter.getText().toString();
+        int count = Integer.parseInt(text);
+        count++;
+        counter.setText((count + ""));
+
+        switch (view.getId()){
+            case R.id.cheeseCakeAdd:
+                Toast.makeText(this, "cheese", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.chocoCakeAdd:
+                Toast.makeText(this, "Chocolate", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.spongeCakeAdd:
+                Toast.makeText(this, "Sponge", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.carrotCakeAdd:
+                Toast.makeText(this, "Carrot", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.redVelvetAdd:
+                Toast.makeText(this, "Red Velvet", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     public void switchPic(View view) {
